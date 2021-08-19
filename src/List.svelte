6d4c7b50ce0d5aa9b0a58533c2dcb3255523caa6
Giovanni <gionneves@gmail.com>
2021-08-19 13:06:50 -0300

    Create password lists

---
+++ src/List.svelte
@@ -0,0 +1,26 @@
+<script>
+    let password = "Passwords";
+</script>
+
+<div id="List">
+    <div class="box">
+        <p>{password}</p>
+    </div>
+</div>
+
+<style lang="scss">
+    .box {
+        background-color: #eee;
+        margin: 8em;
+        padding: 1em;
+        border: #000 1px solid;
+        box-shadow: 2px;
+        border-radius: 0.5em;
+
+        p {
+            text-align: center;
+            font-weight: bold;
+            font-size: x-large;
+        }
+    }
+</style>
